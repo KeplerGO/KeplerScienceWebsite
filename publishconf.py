0ddf0dbeb2a978cae380343e7c079a76804ea85a
Tom Barclay <mrtommyb>
2015-04-24 16:04:13 -0700

    first commit

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://keplerscience.arc.nasa.gov/blog'
+#SITEURL = 'http://keplerscience.arc.nasa.gov/blog'
+SITEURL = ''
 RELATIVE_URLS = False
 
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
