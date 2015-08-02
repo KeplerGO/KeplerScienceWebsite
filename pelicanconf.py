43822443229149a3846cff7778b5bb9280fb4032
Geert Barentsen <hello@geert.io>
2015-08-01 18:37:33 -0700

    Adds a top menu and sidebar navigation

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
+
+# Temporary setting while playing with config:
+LOAD_CONTENT_CACHE = False
 
 AUTHOR = u'Thomas Barclay'
-SITENAME = u'Kepler and K2'
-BANNER_SUBTITLE = u"Guest Observer Office Blog"
+SITENAME = u'Kepler &amp; K2'
+BANNER_SUBTITLE = u"Science Website"
 SITEURL = ''
 SITELOGO = 'images/NASA_logo_vector_lg.png'
 SITELOGO_SIZE = 32
@@ -16,7 +20,7 @@ TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = u'en'
 
-THEME = "themes/pelican-bootstrap3"
+THEME = "themes/pelican-bootstrap3-kepler"
 BOOTSTRAP_THEME = 'flatly'
 BOOTSTRAP_FLUID = False
 
@@ -38,11 +42,32 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
+"""
 LINKS = (('Subscribe', 'http://keplerscience.arc.nasa.gov/NewsExploder.shtml'),
 		 ('Kepler Science Center', 'http://keplerscience.arc.nasa.gov/'),
          ('K2 Science Center', 'http://keplerscience.arc.nasa.gov/K2'),
          ('Mission Manager Updates', 'http://www.nasa.gov/mission_pages/kepler/news/mmu.html'),
          ('Old Blog Posts','http://keplerscience.arc.nasa.gov/Blog.shtml'))
+"""
+DISPLAY_BREADCRUMBS = True
+HIDE_SIDEBAR = True
+MD_EXTENSIONS = (['toc'])
+
+PLUGIN_PATHS = [os.path.join(os.path.dirname(os.path.realpath(__file__)), "plugins")]
+PLUGINS = ['extract_toc']
+
+MENUITEMS = (
+        ('News', '/'),
+        ('Mission', (
+            ('Science', 'BROKEN'),
+            ('Submenu 2', 'BROKEN'),
+            ('Submenu 3', 'BROKEN'),
+            )
+        ),
+        ('K2 Observing', 'pages/k2-observing.html'),
+        ('Data analysis', 'BROKEN'),
+        ('Helpdesk', 'BROKEN'),
+        )
 
 SHOW_ARTICLE_AUTHOR = True
 
