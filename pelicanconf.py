3036e57ebba423954f9fa3c94a00b1189128db51
Geert Barentsen <geert@barentsen.be>
2017-05-03 15:39:25 -0700

    Update C11 release date. This time it will be right, we hope!

--- pelicanconf.py
+++ pelicanconf.py
@@ -124,7 +124,7 @@ IMPORTANT_DATES = (
             ('<b>18 May 2017</b>',
              'K2 Campaign 15 DDT deadline',
              'k2-ddt.html'),
-            ('<b>22 May 2017</b>',
+            ('<b>6 Jun 2017</b>',
              'K2 Campaign 11 calibrated data release (expected)',
              'k2-fields.html'),
          )
