c46cd94921360da9afc9f05c5fcc7f97a252e02b
Geert Barentsen <geert@barentsen.be>
2017-06-30 13:02:32 -0700

    announce aug as expected C12 data release date

--- pelicanconf.py
+++ pelicanconf.py
@@ -118,6 +118,9 @@ KEY_INFORMATION = (
 
 # Defines the "important dates" box on the front page
 IMPORTANT_DATES = (
+            ('<b>Aug 2017</b>',
+             'K2 Campaign 12 calibrated data release (expected)',
+             'k2-fields.html'),
             ('<b>31 Aug 2017</b>',
              'K2 Campaign 16 DDT deadline',
              'k2-ddt.html'),
